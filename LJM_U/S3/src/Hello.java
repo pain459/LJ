9560de57e19d5543bd7f08a063cf71346619bc06
Ravi <ravi.ameer@gmail.com>
2022-01-09 19:50:53 +0530

    Introduced variables.

--- LJM_U/S3/src/Hello.java
+++ LJM_U/S3/src/Hello.java
@@ -2,6 +2,10 @@ public class Hello {
 
     public static void main(String[] args) {
         String name = "Pain";
+        int myFirstNumber = 368 + 1;
+        int mySecondNumber = (10 + 5) + (2 * 10);
         System.out.println("Hello " + name + "!");
+        System.out.println("The number is " + myFirstNumber);
+        System.out.println("The second number is " + mySecondNumber);
     }
 }
